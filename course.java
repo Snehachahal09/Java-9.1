1739a1b7e9f4b1fa7d14362c3a2799f0eeb2a17f
Snehachahal09 <145627843+Snehachahal09@users.noreply.github.com>
2025-11-04 23:18:01 +0530

    Create course

---
+++ course.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class Course {
+
+    private String courseName;
+
+    public Course(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public void showCourse() {
+        System.out.println("Enrolled Course: " + courseName);
+    }
+}
